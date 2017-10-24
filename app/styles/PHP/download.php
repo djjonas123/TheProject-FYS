9f935b8e9f6d02dbe4690a3d124304fa0ace3ac7
Micah Bredenhorst <32055847+Tenriez@users.noreply.github.com>
2017-10-24 13:32:03 +0200

    Add files via upload
    Back-end: database controller en download PHP code.

---
+++ app/styles/PHP/download.php
@@ -0,0 +1,20 @@
+<?php
+if(isset($_REQUEST["file"])){
+    $file = urldecode($_REQUEST["file"]);
+    $filepath = $file;
+    
+    // Process download
+    if(file_exists($filepath)) {
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename="'.basename($filepath).'"');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize($filepath));
+        flush();
+        readfile($filepath);
+        exit;
+    }
+}
+?>
