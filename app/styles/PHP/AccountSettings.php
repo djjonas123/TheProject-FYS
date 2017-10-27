9d5a5033b4e47819100cc554942b2970740272ab
Fabian <Fabian@Fabian-PC.home>
2017-10-27 15:10:31 +0200

    AccountSettings #myPage gefixed

--- app/styles/PHP/AccountSettings.php
+++ app/styles/PHP/AccountSettings.php
@@ -193,7 +193,7 @@ if (!empty($_POST["change_pw"])) {
 </div>
 
 <footer id="bottomClass" class="text-center">
-    <a class="up-arrow" href="index.php.html" data-toggle="tooltip" title="TO TOP">
+    <a class="up-arrow" href="#myPage" data-toggle="tooltip" title="TO TOP">
         <span class="glyphicon glyphicon-chevron-up"></span>
     </a><br><br>
     <p>Powered by Corendon Airlines</p>
