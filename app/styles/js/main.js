db89984f3c216dce5ce33253b7ef90edb2e11ce7
Micah Bredenhorst <32055847+Tenriez@users.noreply.github.com>
2017-10-27 18:17:36 +0200

    Update main.js
    fix

--- app/styles/js/main.js
+++ app/styles/js/main.js
@@ -4,7 +4,7 @@ Javascript for the map.
  */
 
 function myMap() {
-    var myCenter = new google.maps.LatLng(36.856391, 30.874728);
+    var myCenter = new google.maps.LatLng(52.359212, 4.909740000000056);
     var mapProp = {center:myCenter, zoom:12, scrollwheel:false, draggable:false, mapTypeId:google.maps.MapTypeId.ROADMAP};
     var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
     var marker = new google.maps.Marker({position:myCenter});
