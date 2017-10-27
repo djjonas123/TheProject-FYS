09845cf099ac73fe18ebfe98a005f8113ecaed29
Micah Bredenhorst <32055847+Tenriez@users.noreply.github.com>
2017-10-27 18:12:39 +0200

    Update main.js
    nieuwe coördinaten toegevoegd

--- app/styles/js/main.js
+++ app/styles/js/main.js
@@ -4,7 +4,7 @@ Javascript for the map.
  */
 
 function myMap() {
-    var myCenter = new google.maps.LatLng(52.359103, 4.907706400000052);
+    var myCenter = new google.maps.LatLng(36.856391, 30.874728);
     var mapProp = {center:myCenter, zoom:12, scrollwheel:false, draggable:false, mapTypeId:google.maps.MapTypeId.ROADMAP};
     var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
     var marker = new google.maps.Marker({position:myCenter});
